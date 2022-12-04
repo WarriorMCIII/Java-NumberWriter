11dd6b0159af212a4ec1b942e7d4508bc41c00c2
WarriorMCIII <115409064+WarriorMCIII@users.noreply.github.com>
2022-12-04 20:48:07 +0100

    Delete Main.java

--- numberWriter/Main.java
+++
@@ -1,8 +0,0 @@
-package com.hasan.numberWriter;
-
-public class Main {
-	public static void main(String[] args) {
-		int input = 120000000;
-		System.out.println(NumberWriter.writeNumber(input));
-	}
-}
